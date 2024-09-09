0be2217a37d776ae449e064e480db5ff5b11a54e
magda <magdasaif3@gmail.com>
2024-09-09 11:14:21 +0300

    change middleware trait function to be static

--- app/Traits/MiddlewareTrait.php
+++ app/Traits/MiddlewareTrait.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Config;
 trait MiddlewareTrait
 {
     //===================================================================================
-    public function checkMiddlewareApply(){
+    public static function checkMiddlewareApply(){
         $middleware_setting=Config::get('category.middleware');
         if($middleware_setting['allow_middleware']){
             return $middleware_setting['list_of_middlewares'];
